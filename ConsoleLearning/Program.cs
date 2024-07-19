479fd4b168d7ee8ed64f896f49fee63c6dd881fa
toffeantyri <toffeantyri@gmail.com>
2024-07-19 19:11:26 +0500

    Добавьте файлы проекта.

---
+++ ConsoleLearning/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleLearning
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = 0;
+
+            while (a < 100) {
+                Console.WriteLine(a++);
+            }
+
+            string b = "" ;
+
+            while (b != "exit") {
+                Console.WriteLine("Input Exit for End Application");
+                b = Console.ReadLine().ToLower();
+            }
+
+        }
+    }
+}
